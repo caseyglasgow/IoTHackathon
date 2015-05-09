a6a5090d9b7b563c117cd96d55d360042c28c78e
Casey <caseyglasgow@gmail.com>
2015-05-09 18:36:02 +0000

    switched node-red code to index.js

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,34 @@
 var express = require('express');
 var router = express.Router();
+var http = require('http');
+var express = require("express");
+var RED = require("node-red");
+
+var app = express();
+
+app.use("/", express.static("public"));
+
+var server = http.createServer(app);
+
+var settings = {
+    httpAdminRoot:"/red",
+    httpNodeRoot: "/api",
+    userDir:"/home/nol/.nodered/",
+    functionGlobalContext: { }    // enables global context
+};
+
+// Initialise the runtime with a server and settings
+RED.init(server,settings);
+
+app.use(settings.httpAdminRoot,RED.httpAdmin);
+
+// Serve the http nodes UI from /api
+app.use(settings.httpNodeRoot,RED.httpNode);
+
+server.listen(1880);
+
+// Start the runtime
+RED.start();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
