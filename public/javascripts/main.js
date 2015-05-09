bcafe59310829c0a65efdfcfdd0d420f51f3e675
jonrsleep <jsleep@calpoly.edu>
2015-05-09 15:16:01 -0700

    works + setInterval

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,25 +1,22 @@
-var gotData = function(result, status, xhr) {
-	console.log("Got Result");
-} 
 
 var url = 'http://23.253.209.32:1880';
 
 $(document).ready(function(){
    console.log("Loaded");
 	console.log("test");
-   $("button").click(function(){
-    var motionText, lightText;
-      $.get(url + '/motion', function(data, status){
-         motionText = "Motion: "
-         $("#motion").text(motionText.concat(parseInt(data) ? "ON" : "OFF"));
-      });
-      $.get(url + '/light', function(data, status){
-         lightText = "Light: ";
-         $("#light").text(lightText.concat(parseFloat(data) > .5 ? "ON" : "OFF"));
-      $.get(urlReq, function(data, status){
-         console.log("got data" + data);
-      });
-   });
+   setInterval(updateSensors, 1000);
   
 });
+
+var updateSensors = function() {
+   var motionText, lightText;
+   $.get(url + '/motion', function(data, status){
+      motionText = "Motion: "
+      $("#motion").text(motionText.concat(parseInt(data) ? "ON" : "OFF"));
+   });
+   $.get(url + '/light', function(data, status){
+      lightText = "Light: ";
+      $("#light").text(lightText.concat(parseFloat(data) > .5 ? "ON" : "OFF"));
+    });
+}
 
