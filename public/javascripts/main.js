e797a3fc9eff04c9d6b04a8718786f12ac3718e2
Matthew Smith <matthewksmith12@gmail.com>
2015-05-09 11:05:23 -0700

    embedded red?

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,3 +1,33 @@
+var http = require('http');
+var express = require("express");
+var RED = require("node-red");
+
+var app = express();
+
+app.use("/", express.static("public"));
+
+var server = http.createServer(app);
+
+var settings = {
+    httpAdminRoot:"/red",
+    httpNodeRoot: "/api",
+    userDir:"/home/nol/.nodered/",
+    functionGlobalContext: { }    // enables global context
+};
+
+// Initialise the runtime with a server and settings
+RED.init(server,settings);
+
+app.use(settings.httpAdminRoot,RED.httpAdmin);
+
+// Serve the http nodes UI from /api
+app.use(settings.httpNodeRoot,RED.httpNode);
+
+server.listen(1880);
+
+// Start the runtime
+RED.start();
+
 var gotData = function(result, status, xhr) {
 	console.log(result);
 } 
