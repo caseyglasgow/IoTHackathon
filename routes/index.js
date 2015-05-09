d32866fab9562ef4296e5fad866d19e970379c6b
Casey Glasgow <caseyglasgow@pcp068712pcs.wireless.calpoly.edu>
2015-05-08 21:50:46 -0700

    Hello

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+var options = {
+    host: 'somesite.com',
+    port: 443,
+    path: '/some/path',
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json'
+    }
+};
+
+console.log("Test");
+
 module.exports = router;
