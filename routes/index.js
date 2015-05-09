cf933aa2c1ebbc791f181c3f738002ad6c649920
Casey Glasgow <caseyglasgow@pcp068712pcs.wireless.calpoly.edu>
2015-05-09 13:33:26 -0700

    j

--- routes/index.js
+++ routes/index.js
@@ -3,21 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'TITLE' });
+  res.render('index', { title: 'Motion Sensor' });
 
 
   console.log("test");
 });
 
-var options = {
-    host: 'somesite.com',
-    port: 443,
-    path: '/some/path',
-    method: 'GET',
-    headers: {
-        'Content-Type': 'application/json'
-    }
-};
 
 
 
