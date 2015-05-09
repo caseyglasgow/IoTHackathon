5364eefba1526ba3e435bcb21b69bb0e4a301642
Casey Glasgow <caseyglasgow@pcp068712pcs.wireless.calpoly.edu>
2015-05-09 09:44:31 -0700

    l

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,22 +1,28 @@
-var options = {
-    host: '23.253.209.32',
-    port: 8080,
-    path: 'domain2/endpoints/New-Team/3202/0/5600?sync=true',
-    method: 'GET',
-    headers: {
-        'Content-Type': 'application/json'
-    }
-};
+var urlReq = 'http://smartobjectservice.com:8080/domain2/endpoints/New-Team/3311/0/5706?sync=true';
 
-var url = 'smartobjectservice.com:8080/domain2/endpoints/New-Team/3202/0/5600?sync=true'
+var gotData = function(xhr, status) {
+   console.log(status);
+}
+
+var options = {
+    url: urlReq,
+    
+    type: 'GET',
+    Content-Type: 'application/json',
+    username: 'demo',
+    password: 'secret'
+    complete: gotData
+ }
+
+
+var urlReq = 'http://smartobjectservice.com:8080/domain2/endpoints/New-Team/3311/0/5706?sync=true'
 
 $(document).ready(function(){
    console.log("Loaded");
 
    $("button").click(function(){
-      $.get(url, function(data, status){
-         alert("Data: " + data + "\nStatus: " + status);
-      })
+
+      $.ajax(options)
    })
   
 });
