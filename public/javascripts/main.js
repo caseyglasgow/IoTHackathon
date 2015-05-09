e690c012cc7e76078f4e732f244da370c501aef0
jonrsleep <jsleep@calpoly.edu>
2015-05-09 14:31:49 -0700

    Light/ Motion Sensor print

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,7 +1,7 @@
 var gotData = function(result, status, xhr) {
 	console.log("Got Result");
 } 
-var urlReq = 'http://23.253.209.32:1880/motion'
+var url = 'http://23.253.209.32:1880';
 
 
 
@@ -10,9 +10,14 @@ $(document).ready(function(){
    console.log("Loaded");
 	console.log("test");
    $("button").click(function(){
-      $.get(urlReq, function(data, status){
-         console.log("got data" + data);
-
+    var motionText, lightText;
+      $.get(url + '/motion', function(data, status){
+         motionText = "Motion: "
+         $("#motion").text(motionText.concat(parseInt(data) ? "ON" : "OFF"));
+      });
+      $.get(url + '/light', function(data, status){
+         lightText = "Light: ";
+         $("#light").text(lightText.concat(parseFloat(data) > .5 ? "ON" : "OFF"));
       });
    });
   
