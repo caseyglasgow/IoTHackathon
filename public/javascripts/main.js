7085e3d18fe509dce4c73fd83c363792ecf726f5
Casey Glasgow <caseyglasgow@pcp068712pcs.wireless.calpoly.edu>
2015-05-09 09:20:00 -0700

    a

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,9 +1,14 @@
+var options = {
+    host: '23.253.209.32',
+    port: 8080,
+    path: 'domain2/endpoints/New-Team/3202/0/5600?sync=true',
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json'
+    }
+};
+
 $(document).ready(function(){
-   $('img').click(function(){
-      
-      $(this).css({
-           
-      });
-      
-   });
+   console.log("Loaded");
+  
 });
