fcc67f86ec15a6712168e76a134c0750d364dc33
jonrsleep <jsleep@calpoly.edu>
2015-05-09 14:45:45 -0700

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	public/javascripts/main.js
    	views/index.jade

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,20 +1,13 @@
 var gotData = function(result, status, xhr) {
 	console.log("Got Result");
 } 
-<<<<<<< HEAD
+
 var url = 'http://23.253.209.32:1880';
-=======
-var urlReq = 'http://23.253.209.32:1880/light';
->>>>>>> origin/master
-
-
-
 
 $(document).ready(function(){
    console.log("Loaded");
 	console.log("test");
    $("button").click(function(){
-<<<<<<< HEAD
     var motionText, lightText;
       $.get(url + '/motion', function(data, status){
          motionText = "Motion: "
@@ -23,11 +16,8 @@ $(document).ready(function(){
       $.get(url + '/light', function(data, status){
          lightText = "Light: ";
          $("#light").text(lightText.concat(parseFloat(data) > .5 ? "ON" : "OFF"));
-=======
       $.get(urlReq, function(data, status){
-         console.log("got data " + data);
-
->>>>>>> origin/master
+         console.log("got data" + data);
       });
    });
   
