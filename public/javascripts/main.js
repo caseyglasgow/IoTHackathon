e8263b6a145ef5c39627690d16ffdd5e9519fccf
Matthew Smith <matthewksmith12@gmail.com>
2015-05-09 16:06:03 -0700

    added motion average

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,5 +1,6 @@
 
 var url = 'http://23.253.209.32:1880';
+var motionVals = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
 
 $(document).ready(function(){
    console.log("Loaded");
@@ -8,11 +9,30 @@ $(document).ready(function(){
   
 });
 
+var updateSpread = function(data) {
+   var i = 0;
+   for (i = 0; i < motionVals.length; i++) {
+      motionVals[i] = motionVals[i + 1];
+   }
+   motionVals[motionVals.length - 1] = data;
+   var sum = 0;
+   for (i = 0; i < motionVals.length; i++) {
+      sum += motionVals[i];
+   }
+
+   spread = sum / motionVals.length;
+   console.log(motionVals);
+   console.log(spread);
+   
+   return spread;
+}
+
 var updateSensors = function() {
    var motionText, lightText;
    $.get(url + '/motion', function(data, status){
       motionText = "Motion: "
-      $("#motion").text(motionText.concat(parseInt(data) ? "ON" : "OFF"));
+      var aveMotion = updateSpread(parseInt(data));
+      $("#motion").html(motionText.concat((parseInt(data) ? "ON&nbsp;" : "OFF") + " &nbsp; &nbsp; &nbsp;AVG: " + aveMotion));
    });
    $.get(url + '/light', function(data, status){
       lightText = "Light: ";
