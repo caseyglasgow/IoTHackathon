198b2f96fe4784dcee25652f6e6cb2c98cacc00f
calvwng <calvwng@gmail.com>
2015-05-09 15:33:13 -0700

    Adding Bonsai graphics overlay page

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,22 +1,18 @@
-
 var url = 'http://23.253.209.32:1880';
 
 $(document).ready(function(){
    console.log("Loaded");
-	console.log("test");
    setInterval(updateSensors, 1000);
-  
 });
 
 var updateSensors = function() {
    var motionText, lightText;
    $.get(url + '/motion', function(data, status){
-      motionText = "Motion: "
-      $("#motion").text(motionText.concat(parseInt(data) ? "ON" : "OFF"));
+      $("#motionValue").text(parseInt(data) ? "YES" : "NO");
    });
    $.get(url + '/light', function(data, status){
       lightText = "Light: ";
-      $("#light").text(lightText.concat(parseFloat(data) > .5 ? "ON" : "OFF"));
+      $("#lightValue").text(parseFloat(data) > .5 ? "YES" : "NO");
     });
 }
 
